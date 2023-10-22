aa8a322dcc456c149f04d6d0eaccd42240bce3bb
Srikant <kumar_srikant@outlook.com>
2023-10-22 12:21:05 +0530

    Admin table view

--- shopnest/admin.py
+++ shopnest/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 # Register your models here.
 from .models import Cart, CartItem, Category, Order, Product
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price', 'category', 'image_url_link')
+    list_filter = ('category',)
+
+    def image_url_link(self, obj):
+        return format_html('<a href="{}" target="_blank">{}</a>', obj.image_url, obj.image_url)
+
+    image_url_link.short_description = 'Image URL'
+
+admin.site.register(Product, ProductAdmin)
+
+
 admin.site.register(Category)
-admin.site.register(Product)
+# admin.site.register(Product)
 admin.site.register(Cart)
 admin.site.register(CartItem)
 admin.site.register(Order)
