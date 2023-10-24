f1b4642595ab4df77ff5961a0c1e24f5e90d299b
Srikant <kumar_srikant@outlook.com>
2023-10-24 11:37:35 +0530

    models addition

--- shopnest/models.py
+++ shopnest/models.py
@@ -1,5 +1,11 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
+from django.utils.crypto import get_random_string
+
+
+def generate_order_number():
+    return get_random_string(length=20)
 
 
 class Category(models.Model):
@@ -28,9 +34,77 @@ class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
 
-class Order(models.Model):
+    def __str__(self) -> str:
+        return self.product.name
+
+
+
+class ShippingAddress(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    items = models.ManyToManyField(CartItem)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
-    shipping_address = models.TextField()
-    order_date = models.DateTimeField(auto_now_add=True)
+    label = models.CharField(max_length=100, help_text="e.g., Home, Work")
+    street_address = models.TextField()
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.label
+
+
+class Order(models.Model):
+    ORDER_STATUS_CHOICES = (
+        ('Pending', 'Pending'),
+        ('Processing', 'Processing'),
+        ('Shipped', 'Shipped'),
+        ('Delivered', 'Delivered'),
+        ('Canceled', 'Canceled'),
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    items = models.ManyToManyField('CartItem')
+    order_number = models.CharField(max_length=20, default=generate_order_number)
+    order_date = models.DateTimeField(default=timezone.now)
+    shipping_address = models.ForeignKey(ShippingAddress, on_delete=models.SET_NULL, null=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2,default=0)
+    order_status = models.CharField(
+        max_length=20,
+        choices=ORDER_STATUS_CHOICES,
+        default='Pending',
+    )
+
+    def __str__(self):
+        return self.order_number
+
+    def save(self, *args, **kwargs):
+        # Calculate total_price based on items in the order
+        self.total_price = sum(item.product.price * item.quantity for item in self.items.all())
+        super(Order, self).save(*args, **kwargs)
+
+
+class ReviewAndRating(models.Model):
+    user_profile = models.ForeignKey("authentication.UserProfile", on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField()
+    review = models.TextField(blank=True)
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"Rating: {self.rating} for {self.product.name}"
+
+
+class PaymentMethod(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    card_number = models.CharField(max_length=16)
+    expiration_date = models.DateField()
+    cardholder_name = models.CharField(max_length=100)
+    billing_address = models.TextField()
+
+    def __str__(self):
+        return f"**** **** **** {self.card_number[-4:]}"
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    products = models.ManyToManyField('Product')
+
+    def __str__(self):
+        return self.name
