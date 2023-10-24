be0d6bb573d29968c938bf88dd7891d5c28d11b5
Srikant <kumar_srikant@outlook.com>
2023-10-24 11:39:03 +0530

    API's for Order models with serializers and urls mapping

--- shopnest/urls.py
+++ shopnest/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 
-from .views import ProductListCreateView, ProductRetrieveUpdateDestroyView
+from .views import (
+    OrderCreateView,
+    OrderListView,
+    ProductListCreateView,
+    ProductRetrieveUpdateDestroyView,
+)
 
 urlpatterns = [
     path('products/', ProductListCreateView.as_view(), name='product-list-create'),
     path('products/<int:pk>/', ProductRetrieveUpdateDestroyView.as_view(), name='product-detail'),
+    path('orders/create/', OrderCreateView.as_view(), name='order-create'),
+    path('orders/', OrderListView.as_view(), name='order-list'),
 ]
